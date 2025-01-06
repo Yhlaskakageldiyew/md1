3326ad5f89fd95f420e78211cee688e6cc517182
Yhlas <kakageldiyewyhlas74@gmail.com>
2025-01-06 20:21:08 +0500

    3

--- database/migrations/2025_01_06_134341_create_students_table.php
+++ database/migrations/2025_01_06_134341_create_students_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('name2');
             $table->string('surname');
             $table->timestamps();
         });
