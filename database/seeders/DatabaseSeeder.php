c3b786f4455739077f6b0ce680160234badf15a2
Yhlas <kakageldiyewyhlas74@gmail.com>
2025-01-06 20:18:01 +0500

    1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Lesson;
+use App\Models\Registration;
+use App\Models\Student;
+use App\Models\Teacher;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(CourseSeeder::class);
+        $this->call(LocationSeeder::class);
+        $this->call(SeasonSeeder::class);
+
+        Student::factory(100)->create();
+        Teacher::factory(10)->create();
+        Lesson::factory(30)->create();
+        Registration::factory(200)->create();
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
     }
 }
